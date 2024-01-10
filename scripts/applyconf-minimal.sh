e4a1fabe0603f6138f06e9267c41153645f0d691
João Antão <jp.antao@campus.fct.unl.pt>
2024-01-10 14:18:57 +0000

    Update applyconf-minimal.sh

--- scripts/applyconf-minimal.sh
+++ scripts/applyconf-minimal.sh
@@ -3,6 +3,5 @@
 config="${HOME}/.config"
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 ln -sf $(pwd)/zsh/zshrc_minimal ${HOME}/.zshrc
 ln -sf $(pwd)/tmux/tmux.conf ${HOME}/.tmux.conf
